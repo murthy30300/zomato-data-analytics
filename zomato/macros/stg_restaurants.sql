d29ef8120841c924085cc833e233e259859b569c
Vishnu Nukala <vishnunukala@Vishnus-MacBook-Air.local>
2026-08-26 11:47:55 +0530

    v0.1

---
+++ zomato/macros/stg_restaurants.sql
@@ -0,0 +1,10 @@
+-- parse the messy dimension (-null, 50+ rating, $200-300, city after last comma)
+select
+    id::number as restaurant_id,
+    name as restaurant_name,
+    trim(coalesce(regexp_substr(city, '[^,]+$'),city)) as city,
+    try_to_decimal(nullif(rating, '--'),3,1) as rating,
+    try_to_number(regexp_substr(cost,'[0-9]+')) as cost_for_two,
+    try_to_number(regexp_substr(rating_count,'[0-9]+')) as rating_count,
+    cuisine, lic_no as license_no
+from {{source('raw' , 'restaurants')}} where try_to_number(id) is not null
